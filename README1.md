1386373e6fcf91e47c310a3f7e1a5b4656409239
Elliott Day <eday39@gmail.com>
2021-02-17 00:21:39 -0700

    finished the entire quiz app except for better accessibility

--- README1.md
+++ README1.md
@@ -1,1 +1,7 @@
-### Boilerplate app structure for Quiz App project
+<h1>Quiz App Project</h1>
+<p>For one of the most challenging projects I have ever done (thus far), I have made a quiz app, fully functional with appropriate design. While the JS code is some 370 lines long, I have painstankingly made sure every detail is accounted for. Please let me know of any recommendations you may have for me!</p>
+
+<h2>Important Things to Note</h2>
+<p>You will notice I made X a global variable. This is to ensure it is permanently changed as a user goes through the quiz, regardless of the scope in question. Also, you will notice I have many event listeners (likely more than necessary). However, I still chose to uphold their frequency because of time constraint.</p>
+
+<p>Last, I did not include HTML wireframe files. I would normally do this, but I opted out of doing this at this particular time for various reasons.</p>
--- app1.js
+++ app1.js
@@ -1,62 +1,367 @@
-/**
- * Example store structure
- */
-const store = {
-  // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
-    },
-    {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
+
+let X = 0;
+
+    function quizInitialize() {
+
+      $('main').html(`
+        <form class="begin-before">
+          <button type="submit" class="begin">Click here to begin</button>
+        </form>`
+        )
+      $('.begin').on('click', function(e) {
+        e.preventDefault();
+        questionOne()
+      })
     }
-  ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
-};
 
-/*
-Technical Requirements:
+    function questionOne() {
 
-- Must include a render() function that conditionally regenerates the view each time the store is updated
-- Include single-purpose template generation functions
-- Include single-purpose event handler functions
-- Call all functions from a jQuery initializing function
-- NOT add additional HTML elements to the boilerplate code’s index.html file (you may add attributes, i.e., classes and ids, to the existing HTML elements, or link stylesheets or additional scripts if necessary)
-- Render answer choices in a <form>
-- Use semantic HTML, along with CSS and jQuery
-- Follow a11y best practices
-- Be fully usable by keyboard (which will be easy enough if you start with a form)
-- Use responsive design
-*/
+      const greatSchism = {
+        'A.': '1054 AD', 
+        'B.': '1133 AD' ,
+        'C.': '1013 AD' ,
+        'D.': '930 AD'
+      }
+
+      $('main').html('<ul><h2>1. When was the Great Schism (also known as the East-West Schism)?</h2></ul>')
+
+      $('main').append(`
+        <form>
+          <ul>
+          <h4>On question 1 out of 5 | Current Score: ${X}/100pts</h4>
+          <br>
+            <button class="choice-correct"> A) ${greatSchism['A.']}</button>
+          <br>
+            <button class="choice"> B) ${greatSchism['B.']}</button>
+          <br>
+            <button class="choice"> C) ${greatSchism['C.']}</button>
+          <br>
+            <button class="choice"> D) ${greatSchism['D.']}</button>
+          <br>
+          </ul>
+        </form>`)
+        
+          $('.choice-correct').on('click', function(e){
+              e.preventDefault();
+              return $('main').append(`
+              <ul>
+                <h3>That's correct! A is the answer. Good job!</h3>
+                <button class="end-of-question">Next Question</button>
+              </ul>`)
+          })
+
+          $('.choice-correct').on('click', function(e){
+            e.preventDefault();
+            return $('ul').on('click', '.end-of-question', function(e){
+              e.preventDefault();
+              questionTwo()
+            })
+          })
+
+          $('.choice-correct').on('click', function(e){
+            e.preventDefault();
+            return X+=20
+        })
+
+          $('.choice').on('click', function(e){
+            e.preventDefault();
+            return $('main').append(`
+            <ul>
+              <h3 class="wrong">Sorry, that's incorrect. The correct answer is A.</h3>
+              <button class="end-of-question">Next Question</button>
+            </ul>`)
+          })
+
+          $('.choice').on('click', function(e){
+            e.preventDefault();
+            return $('ul').on('click', '.end-of-question', function(e){
+              e.preventDefault();
+              questionTwo()
+            })
+          })
+    }
 
 
-// - Design your app using HTML wireframes, which are HTML - and minimal CSS - only versions of the different screens in your app
+
+    function questionTwo() {
+
+      const charlamagne = {
+        'A.': 'Eastern Orthodox Christianity',
+        'B.': 'Roman Catholicism',
+        'C.': 'The Church of England',
+        'D.': 'Unorthodox Judaism'
+      }
+
+      $('main').html('<ul><h2>2. What religion was Charlamagne apart of?</h2></ul>')
+
+      $('main').append(`
+        <form>
+          <ul>
+          <h4>On question 2 out of 5 | Current Score: ${X}/100pts</h4>
+          <br>
+            <button class="choice"> A) ${charlamagne['A.']}</button>
+          <br>
+            <button class="choice-correct"> B) ${charlamagne['B.']}</button>
+          <br>
+            <button class="choice"> C) ${charlamagne['C.']}</button>
+          <br>
+            <button class="choice"> D) ${charlamagne['D.']}</button>
+          <br>
+          </ul>
+        </form>`)
+
+        $('.choice-correct').on('click', function(e){
+          e.preventDefault();
+          return $('main').append(`
+          <ul>
+            <h3>That's correct! B is the answer. Good job!</h3>
+            <button class="end-of-question">Next Question</button>
+          </ul>`)
+      })
+
+      $('.choice-correct').on('click', function(e){
+        e.preventDefault();
+        return $('ul').on('click', '.end-of-question', function(e){
+          e.preventDefault();
+          questionThree()
+        })
+      })
+
+      $('.choice-correct').on('click', function(e){
+        e.preventDefault();
+        return X+=20
+    })
+
+      $('.choice').on('click', function(e){
+        e.preventDefault();
+        return $('main').append(`
+        <ul>
+          <h3 class="wrong">Sorry, that's incorrect. The correct answer is B.</h3>
+          <button class="end-of-question">Next Question</button>
+        </ul>`)
+      })
+
+      $('.choice').on('click', function(e){
+        e.preventDefault();
+        return $('ul').on('click', '.end-of-question', function(e){
+          e.preventDefault();
+          questionThree()
+        })
+      })
 
 
-/********** TEMPLATE GENERATION FUNCTIONS **********/
+    }
 
-// These functions return HTML templates
+    function questionThree() {
+    
+      const tennisCourt = {
+        'A.': 'An oath taken by members of the French Third Estate vowing to establish a Constitution of the Kingdom while in a tennis court',
+        'B.': 'An oath taken by members of the clergy and the nobles to put an end to the French Revolution',
+        'C.': 'An oath taken by peasants across the western seaboard of France to put an end to the feudal estate system',
+        'D.': 'A goal designed to fix the agricultural and fiscal issues of the time'
+      }
 
-/********** RENDER FUNCTION(S) **********/
+      $('main').html('<ul><h2>3. What was the Tennis Court Oath?</h2></ul>')
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
+      $('main').append(`
+        <form>
+          <ul>
+          <h4>On question 3 out of 5 | Current Score: ${X}/100pts</h4>
+          <br>
+            <button class="choice-correct3"> A) ${tennisCourt['A.']}</button>
+          <br>
+            <button class="choice3"> B) ${tennisCourt['B.']}</button>
+          <br>
+            <button class="choice3"> C) ${tennisCourt['C.']}</button>
+          <br>
+            <button class="choice3"> D) ${tennisCourt['D.']}</button>
+          <br>
+          </ul>
+        </form>`)
+        
+          $('.choice-correct3').on('click', function(e){
+              e.preventDefault();
+              return $('main').append(`
+              <ul>
+                <h3>That's correct! A is the answer. Good job!</h3>
+                <button class="end-of-question">Next Question</button>
+              </ul>`)
+          })
 
-/********** EVENT HANDLER FUNCTIONS **********/
+          $('.choice-correct3').on('click', function(e){
+            e.preventDefault();
+            return $('ul').on('click', '.end-of-question', function(e){
+              e.preventDefault();
+              questionFour()
+            })
+          })
 
-// These functions handle events (submit, click, etc)
+          $('.choice-correct3').on('click', function(e){
+            e.preventDefault();
+            return X+=20
+        })
+
+          $('.choice3').on('click', function(e){
+            e.preventDefault();
+            return $('main').append(`
+            <ul>
+              <h3 class="wrong">Sorry, that's incorrect. The correct answer is A.</h3>
+              <button class="end-of-question">Next Question</button>
+            </ul>`)
+          })
+
+          $('.choice3').on('click', function(e){
+            e.preventDefault();
+            return $('ul').on('click', '.end-of-question', function(e){
+              e.preventDefault();
+              questionFour()
+            })
+          })
+    }
+
+
+
+    function questionFour() {
+
+      const theMoors = {
+        'A.': 'Approximately 350 years',
+        'B.': 'Approximately 130-230 years',
+        'C.': 'Approximately 900-1000 years',
+        'D.': 'Approximately 700-800 years'
+      }
+      $('main').html('<ul><h2>4. How long did the Moors occupy the Iberian Peninsula?</h2></ul>')
+
+      $('main').append(`
+          <form>
+            <ul>
+            <h4>On question 4 out of 5 | Current Score: ${X}/100pts</h4>
+            <br>
+              <button class="choice"> A) ${theMoors['A.']}</button>
+            <br>
+              <button class="choice"> B) ${theMoors['B.']}</button>
+            <br>
+              <button class="choice"> C) ${theMoors['C.']}</button>
+            <br>
+              <button class="choice-correct"> D) ${theMoors['D.']}</button>
+            <br>
+            </ul>
+          </form>`)
+          
+            $('.choice-correct').on('click', function(e){
+                e.preventDefault();
+                return $('main').append(`
+                <ul>
+                  <h3>That's correct! D is the answer. Good job!</h3>
+                  <button class="end-of-question">Next Question</button>
+                </ul>`)
+            })
+
+            $('.choice-correct').on('click', function(e){
+              e.preventDefault();
+              return $('ul').on('click', '.end-of-question', function(e){
+                e.preventDefault();
+                questionFive()
+              })
+            })
+
+            $('.choice-correct').on('click', function(e){
+              e.preventDefault();
+              return X+=20
+          })
+
+            $('.choice').on('click', function(e){
+              e.preventDefault();
+              return $('main').append(`
+              <ul>
+                <h3 class="wrong">Sorry, that's incorrect. The correct answer is D.</h3>
+                <button class="end-of-question">Next Question</button>
+              </ul>`)
+            })
+
+            $('.choice').on('click', function(e){
+              e.preventDefault();
+              return $('ul').on('click', '.end-of-question', function(e){
+                e.preventDefault();
+                questionFive()
+              })
+            })
+    }
+
+    function questionFive() {
+
+      const holyRomanEmpire = {
+        'A.': 'Due to the Glorious Revolution',
+        'B.': 'Due to the Napoleonic Wars',
+        'C.': 'Due to the 100 years war',
+        'D.': 'Due to the Black Death'
+      }
+      $('main').html('<ul><h2>5. How was the Holy Roman Empire Dissolved?</h2></ul>')
+
+      $('main').append(`
+        <form>
+        <h4>On question 5 out of 5 | Current Score: ${X}/100pts</h4>
+          <ul>
+          <br>
+            <button class="choice"> A) ${holyRomanEmpire['A.']}</button>
+          <br>
+            <button class="choice-correct"> B) ${holyRomanEmpire['B.']}</button>
+          <br>
+            <button class="choice"> C) ${holyRomanEmpire['C.']}</button>
+          <br>
+            <button class="choice"> D) ${holyRomanEmpire['D.']}</button>
+          <br>
+          </ul>
+        </form>`)
+        
+          $('.choice-correct').on('click', function(e){
+              e.preventDefault();
+              return $('main').append(`
+              <ul>
+                <h3>That's correct! B is the answer. Good job!</h3>
+                <button class="end-of-question">Next Question</button>
+              </ul>`)
+          })
+
+          $('.choice-correct').on('click', function(e){
+            e.preventDefault();
+            return $('ul').on('click', '.end-of-question', function(e){
+              e.preventDefault();
+              endPage()
+            })
+          })
+
+          $('.choice-correct').on('click', function(e){
+            e.preventDefault();
+            return X+=20
+        })
+
+          $('.choice').on('click', function(e){
+            e.preventDefault();
+            return $('main').append(`
+            <ul>
+              <h3 class="wrong">Sorry, that's incorrect. The correct answer is B.</h3>
+              <button class="end-of-question">Next Question</button>
+            </ul>`)
+          })
+
+          $('.choice').on('click', function(e){
+            e.preventDefault();
+            return $('ul').on('click', '.end-of-question', function(e){
+              e.preventDefault();
+              endPage()
+            })
+          })
+    }
+
+    function endPage() {
+
+      $('main').html(`<h2>Your quiz score is: ${X}/100pts</h2>`)
+      $('main').append(`<div class="restart-embody"><button class="restart">Click here to try again!</button></div>`)
+
+      $('.restart').on('click', function(){
+        quizInitialize()
+        X=0;
+      })
+    }
+
+    quizInitialize()
